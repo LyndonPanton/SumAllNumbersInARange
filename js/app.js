b1a3695d740f78529ca8173bc4e45037a145a47c
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-30 17:13:32 +0100

    Added logic to find sum

--- js/app.js
+++ js/app.js
@@ -3,6 +3,37 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function sum(array) {
+		if (!array[0] || !array[1]) {
+			return "Input must not be empty";
+		} else if (/\D/.test(array[0]) || /\D/.test(array[1])) {
+			return "Input must only contain numerical characters";
+		} else if (isNaN(array[0]) || isNaN(array[1])) {
+			return ("Enter two numbers");
+		} else if (array[0] % 1 !== 0 || array[1] % 1 !== 0) {
+			return ("Enter two integers");
+		} else {
+			array[0] = Number(array[0]);
+			array[1] = Number(array[1]);
+
+			let sum = 0;
+
+			array.sort(function(a, b) {
+				if (a < b) {
+					return -1;
+				} else {
+					return 1;
+				}
+			});
+
+			for (let i = array[0]; i <= array[1]; i++) {
+				sum = sum + i;
+			}
+
+			return sum;
+		}
+	}
+
 	function toggle(chevron) {
 		let task = document.getElementById("task");
 
@@ -35,5 +66,7 @@ window.onload = function(event) {
 	let form = document.getElementById("form");
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
+
+		sum([this.children[0].value, this.children[1].value]);
 	});
 };
